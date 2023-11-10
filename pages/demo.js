66af3d5b54b0a1a1e8d49128877d5307cdbc7acc
GABOIESFH <141193745+GABOIESFH@users.noreply.github.com>
2023-11-09 19:14:10 -0600

    Creating project

---
+++ pages/demo.js
@@ -0,0 +1,9 @@
+export default function Demo() {
+    return (
+        <>
+        <main className='h-screen w-screen bg-gray-500'>
+        <h1 className='text-4xl text-center text-white'>Hola Demo</h1>
+        </main>
+        </>
+    )
+    }
