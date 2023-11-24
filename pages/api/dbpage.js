e37c826d4299f440e6da550b668f1bdd3f08e5d3
GABOIESFH <141193745+GABOIESFH@users.noreply.github.com>
2023-11-23 21:22:59 -0600

    Studying use effect and adding button

--- pages/api/dbpage.js
+++ pages/api/dbpage.js
@@ -1,17 +1,21 @@
 import mysql from "mysql2";
 
 export default async function handler(req, res) {
-  const { method, body } = req;
+  const { method, body } = req; //Esto es una abstraccion del requiere
+
+   // configurar conexion de la base de datos que usara para conectarse constantemente
+   const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "pssword",
+    database: "bibliotecacurso",
+  });
+  //Seleccion de metodos HTTPS
   switch (method) {
+    //Hace Get a API
     case "GET":
-      // configurar conexion de la base de datos
-      const connection = mysql.createConnection({
-        host: "localhost",
-        port: "3306",
-        user: "root",
-        password: "pssword",
-        database: "bibliotecacurso",
-      });
+     
       //realizar una consulta a la base de datos
       connection.query(
         "SELECT * FROM alumnos",
@@ -29,5 +33,27 @@ export default async function handler(req, res) {
       //cerrar conexion
       connection.end();
       break;
-  }
-}
+
+        //Hacer post a API
+    case "POST":
+        console.log(body);
+        connection.query(
+        'INSERT INTO alumnos (nombre,apellidos,correo,matricula,edad) VALUES (?,?,?,?,?)',
+        [body.nombre, body.apellidos, body.correo, body.matricula, body.edad],
+        
+        function (err, results, fields) {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: err });
+          } else {
+            console.log(results);
+            res.status(200).json(results);
+          }
+        }
+        )
+      //cerrar conexion
+      connection.end();
+      break;
+  };
+   
+    }
--- pages/testdb.js
+++ pages/testdb.js
@@ -14,14 +14,50 @@ export async function getServerSideProps(){
 }
 
 export default function Testdb({ data }) {
-  //console.log(data)
+  const [loading, setLoading]= useState(false) //inicializacion en falso
+  const [nombre, setNombre]= useState('')
+//1poner boton
+//2click al boton cambie el estado a true
+//3espero 10 segundos
+//4 cambio el estado falso
+
+  
+//time para cambiar el estado del boton de forma autoimatica despues de N milisegundos
+  const handleLoading = () =>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000)
+  }
+
+  //ejemplo de useefect, se ejecuta cuando carga la pagina 
+  //cargara los datos que esten en los corchetes, si hay una variable y se llama al refrescar pagina
+  //carga de nuevo los datos que habia en al variable o bien cada vez que cambie el estado de algo
+  useEffect(()=>{
+    console.log('useEffect')
+  },[])
+
+
+  console.log(data)
   return (
     <>
+    <div className='flex'>
+    {/* boton de carga */}
+    {loading ? (
+      //cuando esta cargando
+      <button className='bg-red-500' disabled>Detener</button> //inicializado como desactivado
+    ):(
+      //cuando no esta cargando
+      <button className='bg-green-500' onClick={handleLoading}>Iniciar</button>//Al presionar el boton se llama a la funcion handleLoading 
+    )
+    }
+
+    </div>
       <main className="h-screen w-screen bg-black">
         <h1 className="text-4xl text-center text-white">{data[0].nombre}</h1>
 
-        {data.map((alumno) => (
-          <div className="flex" key={alumno.id}>
+        {data.map((alumno,i) => (
+          <div className="flex" key={i}>
             <h1 className="text-white">{alumno.nombre}</h1>
             <h2 className="text-xs">{alumno.apellidos}</h2>
           </div>
